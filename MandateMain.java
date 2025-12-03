b54d5965d11807d6be62ae5bc91fa3b652ce6291
Akshay Chaturvedi <114054547+Strikezor@users.noreply.github.com>
2025-12-03 09:34:37 +0530

    Create MandateMain.java

---
+++ MandateMain.java
@@ -0,0 +1,199 @@
+package com.tcs.sbi.main;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Calendar;
+import java.util.Currency;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.commons.logging.LogSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bouncycastle.bcpg.ArmoredOutputStream;
+import org.bouncycastle.bcpg.SymmetricKeyAlgorithmTags;
+import org.bouncycastle.openpgp.PGPEncryptedDataGenerator;
+import org.bouncycastle.openpgp.PGPPublicKey;
+import org.bouncycastle.openpgp.operator.jcajce.JcePGPDataEncryptorBuilder;
+import org.bouncycastle.openpgp.operator.jcajce.JcePublicKeyKeyEncryptionMethodGenerator;
+
+import com.tcs.sbi.launcher.MandateLauncher;
+import com.tcs.sbi.util.MandateUtility;
+
+public class MandateMain implements Runnable {
+
+	int testCounter = 0; 
+	// private CounterClass c = new CounterClass();
+	File zipName;
+	String type;
+
+	public MandateMain(File folderName, String type) {
+		super();
+
+		this.zipName = folderName;
+		this.type = type;
+	}
+
+	private static final Logger log = LogManager.getLogger(MandateLauncher.class);
+
+	public void run() {
+		String srcPath = MandateLauncher.getToProcessPath() + File.separator + type;
+		String zippedPath = MandateLauncher.getZippedPath();
+		String signedPath = MandateLauncher.getSignedPath();
+		String signedFilePath = "";
+		String finalXmlPath = "";
+		String destPath = MandateLauncher.getDestPath();
+		String backUpPath = MandateLauncher.getBackUpPath();
+		PrivateKey privateKey = MandateLauncher.getPrivateKey();
+		PublicKey publicKey = MandateLauncher.getPublicKey();
+		PGPPublicKey pgpPublicKey = MandateLauncher.getPgpPublicKey();
+		
+		
+		try {
+			boolean success = false;
+			File sourceFolder = new File(srcPath);
+			File[] listOfFolders = sourceFolder.listFiles(File::isDirectory);
+
+			log.info("Total number of folders found in source folder for ACH-Mandate to be processed is : "
+					+ (listOfFolders != null ? listOfFolders.length : 0));
+			boolean isZipped = false;
+			boolean isSigned = false;
+			boolean isEncrypted = false;
+//			Zipping the folder contents
+			if (!isZipped) {
+				String newSrcPath = zipName.getAbsolutePath();
+				String zipFilePath = zippedPath + File.separator + zipName.getName() + ".zip";
+				MandateUtility.zipFolder(Paths.get(newSrcPath), Paths.get(zipFilePath));
+				String currDate = new java.text.SimpleDateFormat("ddMMyyyy").format(new java.util.Date());
+				
+				isZipped = true;
+//			Signing the zipped file start
+				
+				if (isZipped) {
+					try {
+						signedFilePath = signedPath + File.separator + zipName.getName() + ".zip";
+						byte[] zipFileBytes = Files.readAllBytes(Paths.get(zipFilePath));
+						Signature signature = Signature.getInstance("SHA256withRSA");
+						signature.initSign(privateKey);
+						signature.update(zipFileBytes);
+						byte[] digitalSignature = signature.sign();
+						String base64ZipContent = Base64.getEncoder().encodeToString(zipFileBytes);
+						String base64Signature = Base64.getEncoder().encodeToString(digitalSignature); 
+						String base64Certificate = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+						String xmlContent = String.format(
+								"<Envelope> \n" +
+							    "  <OrgContent>%s</OrgContent>\n" +
+								"  <Signature>%s</Signature>\n"	 +
+							    "  <Certificate>%s</Certificate>\n" +
+								"</Envelope>",
+								base64ZipContent,
+								base64Signature,
+								base64Certificate
+								);
+						finalXmlPath = signedFilePath.replaceAll("(?i)\\.zip$", ".xml");
+						Files.write(Paths.get(finalXmlPath),xmlContent.getBytes(StandardCharsets.UTF_8));
+						isSigned = true;
+					
+//			Encrypting the zipped file
+					if (isSigned) {
+						try {
+							Files.createDirectories(Paths.get(destPath + File.separator + currDate));
+							String encryptedFilePath = destPath + File.separator + currDate + File.separator
+									+ zipName.getName() + ".zip";
+							
+							PGPEncryptedDataGenerator encGen = new PGPEncryptedDataGenerator(
+									new JcePGPDataEncryptorBuilder(SymmetricKeyAlgorithmTags.AES_256)
+											.setWithIntegrityPacket(true).setSecureRandom(new SecureRandom())
+											.setProvider("BC"));
+
+							encGen.addMethod(
+									new JcePublicKeyKeyEncryptionMethodGenerator(pgpPublicKey).setProvider("BC"));
+
+							try (OutputStream out = new BufferedOutputStream(new FileOutputStream(encryptedFilePath));
+									ArmoredOutputStream armoredOut = new ArmoredOutputStream(out)){
+									OutputStream encOut = encGen.open(armoredOut, new byte[4096]); 
+								Files.copy(Paths.get(finalXmlPath), encOut);
+								encGen.close();
+							} catch (Exception e) {
+								System.out.println(e.getMessage());
+							}
+							
+//							Files.move(Paths.get(finalXmlPath), Paths.get(encryptedFilePath), StandardCopyOption.REPLACE_EXISTING);
+//						System.out.println("Successfully encrypted folder: " + zipName.getName());
+							log.info("Successfully encrypted file: " + zipName.getName());
+							success = true;
+
+//						source backup
+							String backUpString = backUpPath + File.separator + File.separator + "SourceFilesBackUp"
+									+ File.separator + currDate + File.separator + type + File.separator
+									+ zipName.getName() + ".zip";
+							Files.createDirectories(Paths.get(backUpPath + File.separator + File.separator
+									+ "SourceFilesBackUp" + File.separator + currDate + File.separator + type));
+							MandateUtility.createBackUp(zipFilePath, backUpString);
+
+//						output backup
+							backUpString = backUpPath + File.separator + File.separator + "EncryptedFilesBackUp"
+									+ File.separator + currDate + File.separator + zipName.getName() + ".zip";
+							Files.createDirectories(Paths.get(backUpPath + File.separator + File.separator
+									+ "EncryptedFilesBackUp" + File.separator + currDate + File.separator + type));
+							MandateUtility.createBackUp(encryptedFilePath, backUpString);
+							isEncrypted = true;
+						} catch (Exception e) {
+//						System.out.println("Error encrypting the files : " + e.getMessage());
+							log.info("Error encrypting the files : " + e.getMessage());
+						}
+					} else {
+//						encryption failed
+//						System.out.println("Failed to encrypt the files");
+						log.info("Failed to encrypt the files");
+					}
+				} catch(Exception e) {
+					log.info("Failed to sign the files");
+				}
+				} else {
+//					signing failed
+//					System.out.println("Failed to sign the files");
+					log.info("Failed to sign the files");
+				}
+				
+//				deleting files after processing/
+				try {
+					if(success) {
+//						Files.delete(Paths.get(newSrcPath));    
+						MandateUtility.deleteDirectory(Paths.get(newSrcPath));
+						Files.delete(Paths.get(zipFilePath));
+						Files.delete(Paths.get(finalXmlPath));
+					}
+				} catch (Exception e) {
+					log.info("Error while deleting files from process Folders " + e.getMessage());
+				}
+			} else {
+//				zipping failed
+//				System.out.println("Failed to zip the files");
+				log.info("Failed to zip the files");
+			}
+		} catch (Exception e) {
+			e.getMessage();
+		}
+
+	}
+}
